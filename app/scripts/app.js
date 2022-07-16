12379911eff73dcb27cbf3a9575a6cbc81231ed4
Jacob Cunningham <jcun4128@gmail.com>
2022-07-16 14:21:15 -0500

    really terrible sorting

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -119,13 +119,43 @@ const createCard = ({
     <h3>Due day: ${dueDay}</h3>
     <h3>Min pay: ${minPay}</h3>
     <h3>You pay: ${youPay}</h3>
-    <h3>Yearly debt growth: ${yearlyDebtGrowth}</h3>
-    <h3>Monthly debt growth: ${monthlyDebtGrowth}</h3>
-    <h3>Daily debt growth: ${dailyDebtGrowth}</h3>
-    <h3>Real time growth: ${realTimeGrowth}</h3>
+    <h3>Yearly debt growth: $${yearlyDebtGrowth}</h3>
+    <h3>Monthly debt growth: $${monthlyDebtGrowth}</h3>
+    <h3>Daily debt growth: $${dailyDebtGrowth}</h3>
+    <h3>Real time growth: $${realTimeGrowth}</h3>
     <button type="button" id="${id}" class="debt-card-del-btn">x</button>
   </div>`
 );
+
+// https://stackoverflow.com/a/1129270/2710227
+const compare = (a, b) => {
+  if ( a.last_nom < b.last_nom ){
+    return -1;
+  }
+  if ( a.last_nom > b.last_nom ){
+    return 1;
+  }
+  return 0;
+}
+
+const getDebtInfoById = (curData, id) => {
+  let debtInfo = {};
+
+  for (let i = 0; i < curData.length; i++) {
+    const debt = curData[i];
+
+    for (let j = 0; j < debt.length; j++) {
+      const debtField = debt[j];
+
+      if (debtField.name === 'id' && parseInt(debtField.val) === id) {
+        debtInfo = curData[i];
+        break;
+      }
+    }
+  }
+
+  return debtInfo;
+}
 
 const renderCards = () => {
   // check for and render existing entries
@@ -134,13 +164,62 @@ const renderCards = () => {
   if (curData.length) {
     appBody.innerHTML = '';
 
+    // figure out debt growth and sort in descending order (highest debt growth first)
+    // https://stackoverflow.com/a/1069840/2710227
+    // this is gonna be some nasty O log n stuff, well this is like 2n^2 (nested)
+
+    // curData ex schema
+    const debtGrowthByYearTmp = [];  // {id, growth}
+
+    // oof this sucks, example of bad decision propagating downards into future work
+    // the reason this all started is I wanted to lazily check the field by name for the required field
+    // add check
     curData.forEach(debt => {
+      const debtInfo = {};
+      
+      debt.forEach(debtField => {
+        if (debtField.name === 'id') {
+          debtInfo.id = debtField.val;
+        }
+
+        if (debtField.name === 'bal') {
+          debtInfo.bal = debtField.val;
+        }
+
+        if (debtField.name === 'apr') {
+          debtInfo.apr = debtField.val;
+        }
+      });
+
+      debtGrowthByYearTmp.push(debtInfo);
+    });
+
+    const debtGrowthByYear = [];
+
+    debtGrowthByYearTmp.forEach(debt => {
+      debtGrowthByYear.push({
+        ...debt,
+        // https://stackoverflow.com/a/31581206/2710227
+        yearlyDebtGrowth: (parseFloat(debt.bal) * parseFloat(debt.apr * (1/100))).toLocaleString(undefined, {maximumFractionDigits: 2}) // string to float to string lol
+      })
+    });
+
+    // do the sorting
+    debtGrowthByYear.sort(compare).reverse();
+
+    // now (lol) use this to sort curData by this order AND import the growth data oof
+    // OMG I don't even want to think what N^5 or something this is, how many nested forEach loops dang
+    debtGrowthByYear.forEach(debt => {
+      const debtInfo = getDebtInfoById(curData, debt.id);
       const obj = {};
 
       // bad design array of objects, should be just one object
-      debt.forEach(debtField => obj[debtField.name] = debtField.val);
+      debtInfo.forEach(debtField => obj[debtField.name] = debtField.val);
 
-      const card = createCard(obj);
+      const card = createCard({
+        ...obj,
+        yearlyDebtGrowth: debt.yearlyDebtGrowth
+      });
 
       appBody.innerHTML += card;
     });
